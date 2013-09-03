0106ce4bd1e1ba93a7cc557040109aa71837163a
emma-burrows <emma@astele.co.uk>
2013-09-03 16:43:06 +0100

    Added getRepository() method and updated index.jsp.

--- src/main/java/com/eburrows/openshift/GitHub.java
+++ src/main/java/com/eburrows/openshift/GitHub.java
@@ -1,11 +1,32 @@
 package com.eburrows.openshift;
 
+import java.io.IOException;
+
+import org.eclipse.egit.github.core.User;
+
+import org.eclipse.egit.github.core.*;
+import org.eclipse.egit.github.core.service.RepositoryService;
+
 public class GitHub
 {
 
   public GitHub()
   {
-    // TODO Auto-generated constructor stub
   }
 
+  public static String getRepository()
+  {
+    String result = "";
+    RepositoryService service = new RepositoryService();
+    try
+    {
+      for (Repository repo : service.getRepositories("emma-burrows"))
+        result += repo.getName() + " Watchers: " + repo.getWatchers();
+    }
+    catch (IOException e)
+    {
+      result = e.getMessage();
+    }
+    return result;
+  }
 }
